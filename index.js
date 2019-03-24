7b328f2612a05aa37fb3996aba8f192faf44b3b0
srbhss <48798134+srbhss@users.noreply.github.com>
2019-03-24 22:11:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 let assert = require('assert');
 const mkdirp = require('mkdirp');
 const getDirName = require('path').dirname;
-
+const allure = codeceptjs.container.plugins('allure');
 /**
  * Resemble.js helper class for CodeceptJS, this allows screen comparison
  * @author Puneet Kala
@@ -221,7 +221,8 @@ class ResembleHelper extends Helper {
 
         return boundingBox;
     }
-
+{ 
+var enableScreenshotDiffPlugin = true;}
     _getBrowser() {
         if (this.helpers['WebDriver']) {
             return this.helpers['WebDriver'].browser;
